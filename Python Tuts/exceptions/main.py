0df99362affe860d7a8c6859f9f96f0c234787f9
Purab Modi <purabmodi2002@gmail.com>
2023-07-21 18:56:36 +0530

    Completed Python Exceptions

--- Python Tuts/exceptions/main.py
+++ Python Tuts/exceptions/main.py
@@ -1,9 +1,34 @@
 def main():
-    exception()
+    print(get_int())
 
 
+#  else prints n value when there is not exception
 def exception():
-    print("Checking exception")
+    try:
+        n = int(input("Enter the number: "))
+        print(f"The number is {n}")
+    except ValueError:
+        print("The value of n is not correct")
+    else:
+        print(f"The number is {n}")
+
+
+# get the integer even after incorrectinput/exception using while loop
+def get_int():
+    while True:
+        try:
+            return int(input("Enter the number: "))
+        except ValueError:
+            print("Not an Integer")
+
+
+# pass - dont do anything when exception is received
+
+def pass_function():
+    try:
+        return int(input("Enter the number: "))
+    except ValueError:
+        pass
 
 
 main()
